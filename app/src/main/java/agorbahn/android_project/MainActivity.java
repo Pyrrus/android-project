afe9d043af68efa814485a0aadfc0542fbeb5560
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-11-18 09:27:34 -0800

    add menu

--- app/src/main/java/agorbahn/android_project/MainActivity.java
+++ app/src/main/java/agorbahn/android_project/MainActivity.java
@@ -1,8 +1,11 @@
 package agorbahn.android_project;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,4 +20,20 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu,menu);
         return super.onCreateOptionsMenu(menu);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.itemMain:
+                startActivity(new Intent(MainActivity.this,MainActivity.class));
+                Toast.makeText(getApplicationContext(),"Home selected",Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.itemAdd:
+                startActivity(new Intent(MainActivity.this,AddActivity.class));
+                Toast.makeText(getApplicationContext(),"Home selected",Toast.LENGTH_LONG).show();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
 }
+
