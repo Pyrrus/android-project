11b04d8b08b67e5abbf3eba35abac378cf297f67
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-11-18 08:37:30 -0800

    add menu for the app

--- app/src/main/java/agorbahn/android_project/MainActivity.java
+++ app/src/main/java/agorbahn/android_project/MainActivity.java
@@ -2,6 +2,7 @@ package agorbahn.android_project;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
