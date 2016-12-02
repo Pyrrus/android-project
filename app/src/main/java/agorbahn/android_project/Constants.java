57d145abc7b1178ae92b59cf52a7d0284d8810d2
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-12-02 09:00:03 -0800

    Add data in Constants, add key for gradle properties, and update gradle build.

--- app/src/main/java/agorbahn/android_project/Constants.java
+++ app/src/main/java/agorbahn/android_project/Constants.java
@@ -5,4 +5,8 @@ package agorbahn.android_project;
  */
 
 public class Constants {
+    public static final String DOCTOR_APIKEY = BuildConfig.DoctorAPIKEY;
+    public static final String DOCTOR_BASE_URL = "https://api.betterdoctor.com/2016-03-01/doctors";
+    public static final String LOCATION_QUERY_PARAMETER = "location";
+    public static final String APIKEY_QUERY_PARAMETER = "user_key";
 }
---
+++ app/src/main/java/agorbahn/android_project/services/DoctorServices.java
@@ -0,0 +1,9 @@
+package agorbahn.android_project.services;
+
+/**
+ * Created by Adam on 12/2/2016.
+ */
+
+public class DoctorServices {
+
+}
