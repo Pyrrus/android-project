53a62fea52e320dfb30f3c7273c3724ddb4fb95b
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-12-02 08:37:35 -0800

    update gradle with cardview, and add doctor.java

---
+++ app/src/main/java/agorbahn/android_project/models/Doctor.java
@@ -0,0 +1,83 @@
+package agorbahn.android_project.models;
+
+/**
+ * Created by Adam on 12/2/2016.
+ */
+
+import org.parceler.Parcel;
+import java.util.ArrayList;
+
+@Parcel
+public class Doctor {
+    private String mName;
+    private String mSpecialty;
+    private ArrayList<String> mPhone = new ArrayList<>();
+    private double mLatitude;
+    private double mLongitude;
+    private String mStreet;
+    private String mStreet2;
+    private String mCity;
+    private String mState;
+    private String mZip;
+    private String mBio;
+
+    public Doctor() {}
+
+    public Doctor(String mName, String mSpecialty, ArrayList<String> mPhone, double mLatitude, double mLongitude, String mStreet, String mStreet2, String mCity, String mState, String mZip, String mBio) {
+        this.mName = mName;
+        this.mSpecialty = mSpecialty;
+        this.mPhone = mPhone;
+        this.mLatitude = mLatitude;
+        this.mLongitude = mLongitude;
+        this.mStreet = mStreet;
+        this.mStreet2 = mStreet2;
+        this.mCity = mCity;
+        this.mState = mState;
+        this.mZip = mZip;
+        this.mBio = mBio;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public String getSpecialty() {
+        return mSpecialty;
+    }
+
+    public ArrayList<String> getPhone() {
+        return mPhone;
+    }
+
+    public double getLatitude() {
+        return mLatitude;
+    }
+
+    public double getLongitude() {
+        return mLongitude;
+    }
+
+    public String getStreet() {
+        return mStreet;
+    }
+
+    public String getStreet2() {
+        return mStreet2;
+    }
+
+    public String getCity() {
+        return mCity;
+    }
+
+    public String getState() {
+        return mState;
+    }
+
+    public String getZip() {
+        return mZip;
+    }
+
+    public String getBio() {
+        return mBio;
+    }
+}
