c485659a805fb37b49a482128593e5650358c9eb
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-12-12 19:39:19 -0800

    fix the link error

--- app/src/main/java/agorbahn/android_project/UI/CreateAccountActivity.java
+++ app/src/main/java/agorbahn/android_project/UI/CreateAccountActivity.java
@@ -71,7 +71,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
     public void onClick(View view) {
 
         if (view == mLoginTextView) {
-            Intent intent = new Intent(CreateAccountActivity.this, MainActivity.class);
+            Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
