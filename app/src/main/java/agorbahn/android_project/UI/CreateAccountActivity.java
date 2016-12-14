88c677b054eb77ffb39d3644ffa47ebdf93b3640
Adam Gorbahn <adamgorbahn@hotmail.com>
2016-12-13 18:03:46 -0800

    update go back to the login

--- app/src/main/java/agorbahn/android_project/UI/CreateAccountActivity.java
+++ app/src/main/java/agorbahn/android_project/UI/CreateAccountActivity.java
@@ -74,6 +74,7 @@ public class CreateAccountActivity extends AppCompatActivity implements View.OnC
             Intent intent = new Intent(CreateAccountActivity.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
+//            test
             finish();
         }
 
